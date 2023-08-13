19dd379162d5d4250588107597cd7722aff8ab47
Wolves <yangwolves@foxmail.com>
2023-08-13 22:05:26 +0800

    routine

---
+++ 2023/08/Q23.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 李洋 on 2023/8/12.
+//
+#include "../../dataStruct/LinkedList/lists.h"
+
+using namespace std;
+class Q23 {
+public:
+    bool compare(const ListNode *a, const ListNode *b) {
+        return a->val < b->val;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "2023/08/24.cpp"
-#include "lists.h"
+
+using namespace std;
 
 int main() {
-    Q24 *q = new Q24();
-    ListNode *head = createRandomList(4);
-    head->list();
-    return 0;
+
 }
