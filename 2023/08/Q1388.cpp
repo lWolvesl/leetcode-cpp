24771b10437a1c4ee405d8dcc25253193d445db2
Wolves <yangwolves@foxmail.com>
2023-08-19 11:22:29 +0800

    routine

---
+++ 2023/08/Q1388.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 李洋 on 2023/8/18.
+//
+#include <vector>
+
+using namespace std;
+
+class Q1388 {
+public:
+    int maxSizeSlices(vector<int> &slices) {
+
+    }
+};
---
+++ 2023/08/Q2235.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 李洋 on 2023/8/19.
+//
+class Q2235 {
+public:
+    int sum(int num1, int num2) {
+        while (num2) {
+            unsigned int carry = (unsigned int) (num1 & num2) << 1;
+            num1 ^= num2;
+            num2 = carry;
+        }
+        return num1;
+    }
+};
