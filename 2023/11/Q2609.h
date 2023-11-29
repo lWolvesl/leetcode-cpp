2fbe6e991940db8539e9a1f9c051502636211327
Wolves <yangwolves@foxmail.com>
2023-11-29 23:24:30 +0800

    routine

--- 2023/11/Q2609.h
+++ 2023/11/Q2609.h
@@ -11,18 +11,15 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int findTheLongestBalancedSubstring(string s) {
-        int n = s.size(), idx = 0, ans = 0;
-        while (idx < n) {
-            int a = 0, b = 0;
-            while (idx < n && s[idx] == '0' && ++a >= 0) idx++;
-            while (idx < n && s[idx] == '1' && ++b >= 0) idx++;
-            ans = max(ans, min(a, b) * 2);
-        }
-        return ans;
+int findTheLongestBalancedSubstring(string s) {
+    int n = s.size(), idx = 0, ans = 0;
+    while (idx < n) {
+        int a = 0, b = 0;
+        while (idx < n && s[idx] == '0' && ++a >= 0) idx++;
+        while (idx < n && s[idx] == '1' && ++b >= 0) idx++;
+        ans = max(ans, min(a, b) * 2);
     }
-};
+    return ans;
+}
 
 #endif //LEECODE_C_Q2609_H
